d1484dea9e0fa11712f68b1f825d16e7b00a8491
powenn <powen0966091716@gmail.com>
2022-04-13 08:57:28 +0800

    Update py

--- python/get-iOS-DeviceName-and-UDID.py
+++ python/get-iOS-DeviceName-and-UDID.py
@@ -15,9 +15,16 @@ def get_udid_and_name() :
         info = "NO iOS Device connected"
     else:
         for udid in UDID[:-1] :
-            DEVICENAME = subprocess.check_output([IDEVICE_NAME,"-u",f"{udid}"], stderr=subprocess.DEVNULL).decode().split("\n")
-            for devicename in DEVICENAME[:-1]:
-                info += f"iOS Device Name: {devicename}\nUDID: {udid}\n"
+            try:
+                DEVICENAME = subprocess.check_output([IDEVICE_NAME,"-u",f"{udid}"], stderr=subprocess.DEVNULL).decode().split("\n")
+                for devicename in DEVICENAME[:-1]:
+                    info += f"iOS Device Name: {devicename}\nUDID: {udid}\n"
+            except subprocess.CalledProcessError:
+                info = (
+                "Please make sure your iOS device is connected\n"
+                "You may need to accept the trust dialog\n"
+                "on your device before trying again"
+                )
     print(info)
 
 if __name__ == "__main__":
